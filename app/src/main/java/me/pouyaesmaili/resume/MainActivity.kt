2eb69663e848417fd01ba9dcb7dcb1d2f61ecba0
Pouya Esmaili <poesmaili@gmail.com>
2022-11-15 00:15:36 +0330

    Add descriptions

--- app/src/main/java/me/pouyaesmaili/resume/MainActivity.kt
+++ app/src/main/java/me/pouyaesmaili/resume/MainActivity.kt
@@ -1,11 +1,23 @@
 package me.pouyaesmaili.resume
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val callBtn = findViewById<View>(R.id.fab_call) as FloatingActionButton
+        callBtn.setOnClickListener { v ->
+            val phoneIntent = Intent(Intent.ACTION_DIAL)
+            phoneIntent.data = Uri.parse("tel:+989309914798")
+            startActivity(phoneIntent)
+        }
     }
 }
